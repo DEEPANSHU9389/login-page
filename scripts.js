a41d5584c26da4956f4e4fbf25f273d46134fec6
deepanshu <deepanshukumar2022@gmail.com>
2024-10-09 20:40:25 +0530

    final code

--- scripts.js
+++ scripts.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
     $('#loginForm').on('submit', function(event) {
-        event.preventDefault();
-
-        var email = $('#email00@gmail.com').val();
-        var password = $('#password').val();
-
-        if(email === '' || password === '') {
-            alert('Please fill all the fields');
-        } else {
-            alert('Login successful!');
-            // You can send form data to the server using AJAX if needed
-            // $.post('server-endpoint', { email: email00@gmail.com, password: password });
-        }
+        event.preventDefault(); 
+        alert('Login form submitted!'); 
     });
 });
